9ff389f208e4a4000757baa3e7682ee611624645
Calum Meikle <calummeikle2@gmail.com>
2021-02-14 17:41:16 +0000

    Making sure there is a stuctured data transfer

--- LandWaterContent/main.py
+++ LandWaterContent/main.py
@@ -1,11 +1,16 @@
 import pandas as pd
+
 import requests
 import logging
 import io
 
+from pathlib import Path
+
 logger = logging.getLogger(__name__)
 
-def query_api(apikey:str, latitude:str, longitude:str, count=1000:str):
+data_folder = Path("../LandWaterContentData/")
+
+def query_api(apikey:str, latitude:str, longitude:str, count="1000"):
     # Construct the Url to request the dataset
     # TODO: There is a way to pull this down using a Polygon, which may give us more information
     url_csv = f"https://api.planetos.com/v1/datasets/nasa_gldas_lwc_monthly/point?apikey={apikey}&lat={latitude}&origin=dataset-details&lon={longitude}&csv=true&count={count}"
@@ -15,8 +20,9 @@ def query_api(apikey:str, latitude:str, longitude:str, count=1000:str):
         # Read Csv into pandas
         df = pd.read_csv(io.StringIO(data))
         filename = f"Lat{latitude}Lon{longitude}Count{count}.csv"
+        filepath = data_folder.joinpath(filename)
         # And create a csv file in your local directory with the info, so that we don't have to continue querying the API
-        df.to_csv(filename)
+        df.to_csv(filepath)
     except:
         ## TODO update this exception
         logger.error("Something went wrong")
@@ -34,8 +40,9 @@ if __name__ == "__main__":
     # and make sure it is always passed so that the api can read it, ending with .5 possibly
     count = "1000"
     try:
-        filepath = f"Lat{latitude}Lon{longitude}Count{count}.csv"
-        df = read_csv(filename)
+        filename = f"Lat{latitude}Lon{longitude}Count{count}.csv"
+        filepath = data_folder.joinpath(filename)
+        df = read_csv(filepath)
     except FileNotFoundError:
         logger.info("The file has not been found, this is probably because it has not been run, we will run a request to the API")
         query_api(apikey, latitude, longitude, count)
