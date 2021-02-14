d04d11a44c805a22daa65bfb47b49199d88c1a2b
Calum Meikle <calummeikle2@gmail.com>
2021-02-14 20:07:06 +0000

    Got a working retrieval of land water content data

---
+++ LandWaterContent/checking_out_data.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import requests
+import logging
+import io
+import folium
+#import urllib
+
+
+from pathlib import Path
+
+data_folder = Path("../LandWaterContentData/")
+apikey = "c9f1fcccdac44031a945633ce6df8da3"
+
+latitude = "15.5"
+longitude = "-15.5"
+#latitude, longitude = lat_long_rounding(latitude, longitude)
+
+area_of_interest = folium.Map(location = [latitude, longitude], zoom_start= 10.5, control_scale=True)
+# TODO: Need to figure out how to mae this into a pop up
+#area_of_interest.save("area_of_interest.html")
+#urllib.urlopen("area_of_interest.html") Look at my German python file to remind myself how to do this
+count="1000"
+
+filename = f"Lat{latitude}Lon{longitude}Count{count}.csv"
+filepath = data_folder.joinpath(filename)
+df = pd.read_csv(filepath)
+
+plt.plot(pd.to_datetime(df["axis:time"]), df["data:Water_Thickness"])
+plt.show()
--- LandWaterContent/main.py
+++ LandWaterContent/main.py
@@ -12,6 +12,7 @@ logger = logging.getLogger(__name__)
 
 data_folder = Path("../LandWaterContentData/")
 
+
 def query_api(apikey:str, latitude:str, longitude:str, count="1000"):
     """
     The API Query - Parameters passed, your api key, latitude, longitude 
@@ -26,22 +27,15 @@ def query_api(apikey:str, latitude:str, longitude:str, count="1000"):
         data = r.content.decode('utf8')
     except Exception as e:
         logger.error(e)
-    finally:
-        # Read Csv into pandas
-        # TODO: Something is failing at this point, it may be that I have queried the api too much today
-        df = pd.read_csv(io.StringIO(data))
-        print(df)
-        filename = f"Lat{latitude}Lon{longitude}Count{count}.csv"
-        filepath = data_folder.joinpath(filename)
-        # And create a csv file in your local directory with the info, so that we don't have to continue querying the API
-        df.to_csv(filepath)
+    # Read Csv into pandas
+    # TODO: Something is failing at this point and returning empty dfs, it may be that I have queried the api too much today
+    df = pd.read_csv(io.StringIO(data))
 
-def read_csv(filepath):
-    """
-    Read the csv in
-    """
-    df = pd.read_csv(filepath)
-    return df
+    filename = f"Lat{latitude}Lon{longitude}Count{count}.csv"
+    filepath = data_folder.joinpath(filename)
+    # And create a csv file in your local directory with the info, so that we don't have to continue querying the API
+    df.to_csv(filepath)
+
 
 def lat_long_rounding(latitude, longitude):
     """
@@ -52,13 +46,12 @@ def lat_long_rounding(latitude, longitude):
     return latitude, longitude
 
 if __name__ == "__main__":
-    apikey = "c9f1fcccdac44031a945633ce6df8da3&lat"
+    apikey = "c9f1fcccdac44031a945633ce6df8da3"
     
     latitude = "15.5"
     longitude = "-15.5"
     latitude, longitude = lat_long_rounding(latitude, longitude)
-    print(latitude)
-    print(longitude)
+
     # area_of_interest = folium.Map(location = [latitude, longitude], zoom_start= 10.5, control_scale=True)
     # # TODO: Need to figure out how to mae this into a pop up
     # area_of_interest
@@ -71,5 +64,5 @@ if __name__ == "__main__":
             raise("The dataframe is empty, something went wrong when downloading it from the api")
             # TODO: Create a function to remove this datafile from the folder and then query the api
     except FileNotFoundError:
-        logger.info("The file has not been found, this is probably because it has not been run, we will run a request to the API")
+        logger.info("The file has not been found, this is probably because a query of this sort has not been run, we will now query the API")
         query_api(apikey, latitude, longitude, count)
