2a53b1243107261b91e0cb9e1a45b5921306abad
Calum Meikle <calummeikle2@gmail.com>
2021-06-27 14:41:21 +0100

    Project folders that hadn't been committed

--- ERA5/checking_out_data.py
+++ ERA5/checking_out_data.py
@@ -10,7 +10,7 @@ import folium
 
 from pathlib import Path
 
-data_folder = Path("../LandWaterContentData/")
+data_folder = Path("../ERA5Data/")
 apikey = "c9f1fcccdac44031a945633ce6df8da3"
 
 latitude = "15.5"
@@ -18,7 +18,6 @@ longitude = "-15.5"
 #latitude, longitude = lat_long_rounding(latitude, longitude)
 
 area_of_interest = folium.Map(location = [latitude, longitude], zoom_start= 10.5, control_scale=True)
-folium.Marker(latitude, longitude).add_to(area_of_interest)
 # TODO: Need to figure out how to mae this into a pop up
 #area_of_interest.save("area_of_interest.html")
 #urllib.urlopen("area_of_interest.html") Look at my German python file to remind myself how to do this
@@ -27,6 +26,7 @@ count="1000"
 filename = f"Lat{latitude}Lon{longitude}Count{count}.csv"
 filepath = data_folder.joinpath(filename)
 df = pd.read_csv(filepath)
-
-plt.plot(pd.to_datetime(df["axis:time"]), df["data:Water_Thickness"])
+df = df.drop("Unnamed: 0", axis="columns")
+df = df.dropna(how="all")
+plt.plot(pd.to_datetime(df["axis:time0_lat_lon:time"]), df['data:air_temperature_at_2_metres'])
 plt.show()
---
+++ ERA5/main.py
@@ -0,0 +1,68 @@
+import pandas as pd
+
+import requests
+import logging
+import io
+import folium
+
+
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+data_folder = Path("../ERA5Data/")
+
+
+def query_api(apikey:str, latitude:str, longitude:str, count="1000"):
+    """
+    The API Query - Parameters passed, your api key, latitude, longitude 
+    and the number of months you want starting from 2001, default = 1000,
+    giving you the full dataset
+    """
+    # Construct the Url to request the dataset
+    # TODO: There is a way to pull this down using a Polygon, which may give us more information
+    url_csv=f"https://api.planetos.com/v1/datasets/ecmwf_era5_v2/point?apikey={apikey}&lat={latitude}&origin=dataset-details&lon={longitude}&csv=true&count={count}"
+    try:
+        r = requests.get(url_csv)
+        data = r.content.decode('utf8')
+    except Exception as e:
+        logger.error(e)
+    # Read Csv into pandas
+    # TODO: Something is failing at this point and returning empty dfs, it may be that I have queried the api too much today
+    df = pd.read_csv(io.StringIO(data))
+
+    filename = f"Lat{latitude}Lon{longitude}Count{count}.csv"
+    filepath = data_folder.joinpath(filename)
+    # And create a csv file in your local directory with the info, so that we don't have to continue querying the API
+    df.to_csv(filepath)
+
+
+def lat_long_rounding(latitude, longitude):
+    """
+    The resolution of this dataset is 1, so we are always querying an average at .5 latitude and longitude
+    """
+    latitude = latitude.split(".")[0] + ".5"
+    longitude = longitude.split(".")[0] + ".5"
+    return latitude, longitude
+
+if __name__ == "__main__":
+    apikey = "c9f1fcccdac44031a945633ce6df8da3"
+    
+    latitude = "15.5"
+    longitude = "-15.5"
+    latitude, longitude = lat_long_rounding(latitude, longitude)
+
+    # area_of_interest = folium.Map(location = [latitude, longitude], zoom_start= 10.5, control_scale=True)
+    # # TODO: Need to figure out how to mae this into a pop up
+    # area_of_interest
+    count="1000"
+    try:
+        filename = f"Lat{latitude}Lon{longitude}Count{count}.csv"
+        filepath = data_folder.joinpath(filename)
+        df = pd.read_csv(filepath)
+        if df.empty:
+            raise("The dataframe is empty, something went wrong when downloading it from the api")
+            # TODO: Create a function to remove this datafile from the folder and then query the api
+    except FileNotFoundError:
+        logger.info("The file has not been found, this is probably because a query of this sort has not been run, we will now query the API")
+        query_api(apikey, latitude, longitude, count)
